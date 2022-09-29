851ad404f2a2772b0dae6c934c99ba1184200a47
samson-maina <114148322+samson-maina@users.noreply.github.com>
2022-09-29 17:55:49 +0300

    Add files via upload

---
+++ printing numbers.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+int i;
+for(i=234;i>=43;i--)
+{
+cout << i <<"    ";
+}
+
+    return 0;
+}
