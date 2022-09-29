db5ba8e5260fb7c7adf42747f696e9e5eb462625
samson-maina <114148322+samson-maina@users.noreply.github.com>
2022-09-29 18:30:14 +0300

    Add files via upload

---
+++ starting and end point program.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int i,j,k,sum=0;
+cout<<"enterthe starting point"<<endl;
+cin>>j;
+cout<<"enter the end point"<<endl;
+cin>>k;
+for(i=j;i<=k;i++)
+{
+cout<<i<<endl;
+}
+sum=i+2;
+{
+cout<<"sum of the numbers" << j << "and" << k << "is"<<sum;
+}
+return 0;
+}
