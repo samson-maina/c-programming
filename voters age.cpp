75ed8db3ee129adec1e442ac3f9bdff458a2a84a
samson-maina <114148322+samson-maina@users.noreply.github.com>
2022-10-04 09:41:25 +0300

    Add files via upload

---
+++ voters age.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int age;
+cout <<" enter age";
+cin>>age;
+
+if(age>=18)
+{
+cout<<"elligible to vote";
+}
+else 
+{
+cout<<"not elligible to vote";
+}
+return 0;
+}
